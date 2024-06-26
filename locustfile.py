212d00bff511ef91a5b718c3c38e98b982eeef50
Josh Hart <joshcolehart@gmail.com>
2024-06-26 10:10:11 -0700

    test: add loadtesting with locust
    Added loadtesting with locust but had issues with performance so going
    to try Vegeta

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,20 @@
 from datetime import datetime
-from locust import HttpUser, task, between
+from locust import FastHttpUser, task, between 
 import random
 import time
 
-class FastAPIUser(HttpUser):
-    wait_time = between(1, 3)
+class FastAPIUser(FastHttpUser):
+    wait_time = between(3, 7)
     
     def on_start(self):
         self.device_id = self.register_device()
         if self.device_id:
             self.update_device_location() 
-            time.sleep(2) # Adding a delay to ensure the location update is processed
+            time.sleep(2)  # Adding a delay to ensure the location update is processed
 
 
     def register_device(self):
-        """ Helper function to register a device and return the device ID """
+        # Helper function to register a device and return the device ID 
         device_data = {"user_id": f"user-{random.randint(1, 1000)}", "device_name": f"Device-{random.randint(1, 1000)}"}
         response = self.client.post("/api/v1/devices", json=device_data)
         if response.status_code == 201:
@@ -32,7 +32,10 @@ class FastAPIUser(HttpUser):
                 "longitude": random.uniform(-180, 180),
                 "timestamp": datetime.now().isoformat()
             }
-            self.client.post(f"/api/v1/devices/{self.device_id}/locations", json=location_data)
+            response = self.client.post(f"/api/v1/devices/{self.device_id}/locations", json=location_data)
+            if response.status_code != 200:
+                print(f"Failed to update location: {response.status_code} - {response.text}")
+                return None
         else:
             print("No device ID available.")
 
@@ -41,7 +44,7 @@ class FastAPIUser(HttpUser):
         if self.device_id:
             response = self.client.get(f"/api/v1/devices/{self.device_id}/location")
             if response.status_code != 200:
-                print(f"Failed to get current location in get_current_location: {response.status_code} - {response.text}")
+                print(f"Failed to get current location: {response.status_code} - {response.text}")
         else:
             print("No device ID available.")
 
@@ -50,15 +53,12 @@ class FastAPIUser(HttpUser):
         if self.device_id:
             response = self.client.get(f"/api/v1/devices/{self.device_id}")
             if response.status_code != 200:
-                print(f"Failed to get current location in get_device_info: {response.status_code} - {response.text}")
+                print(f"Failed to get device info: {response.status_code} - {response.text}")
         else:
             print("No device ID available.")
 
     @task
     def list_devices(self):
-        if self.device_id:
-            response = self.client.get(f"/api/v1/devices")
-            if response.status_code != 200:
-                print(f"Failed to get current location in get_device_info: {response.status_code} - {response.text}")
-        else:
-            print("No device ID available.")
+        response = self.client.get(f"/api/v1/devices")
+        if response.status_code != 200:
+            print(f"Failed to list devices: {response.status_code} - {response.text}")
