3f0c3305423b26dc934edf28de4ceb9d8a4de6f5
Josh Hart <joshcolehart@gmail.com>
2024-06-26 10:21:50 -0700

    chore: update files to use linters (isort, black)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-import sys
 import os
+import sys
+
 from fastapi import FastAPI
+
 from src.devices.router import router as devices_router
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -11,4 +13,5 @@ app.include_router(devices_router, prefix="/api/v1", tags=["devices"])
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8080)
